e13b9d47f6f395297c0942113ba881d2e582da83
Subhransu Behera <152528962+Behera873@users.noreply.github.com>
2025-12-28 19:58:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "name" {
   ami           = "ami-068c0051b15cdb816"
   instance_type = "t3.micro"
 
-lifecycle {
-    prevent_destroy = true
+tags =  {
+    name= "Subh"
   }
 }
